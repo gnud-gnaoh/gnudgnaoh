6b12b8742c50434fdfae44bd3f3ac17e93fcdd4e
Nguyen Hoang Dung <gnudgnaoh.work@gmail.com>
2025-06-14 15:17:33 +0300

    add underline to current route

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,13 +1,30 @@
+'use client';
+
 import Link from "next/link";
 import Container from "../components/Container";
+import { usePathname } from "next/navigation";
+import styles from './Header.module.css';
 
 export default function Header() {
+  const pathname = usePathname();
+
   return (
     <header>
       <Container>
-        <nav> 
-          <Link href="/">about</Link>
-          <Link href="/posts">posts</Link>
+        <nav className={styles.nav}> 
+          <Link 
+            href="/"
+            className={`${styles.link} ${pathname === '/' ? styles.active : ''}`}
+          >
+            about
+          </Link>
+          
+          <Link 
+            href="/posts"
+            className={`${styles.link} ${pathname === '/posts' ? styles.active : ''}`}
+          >
+            posts
+          </Link>
         </nav>
       </Container>
     </header>
