1711ed9b724a92293ab0f41c3c58c2f949b8cd49
Nguyen Hoang Dung <gnudgnaoh.work@gmail.com>
2025-06-15 13:08:21 +0300

    try to add icon on github pages

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,12 @@
 import '/app/global.css';
 import Header from '../components/Header';
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  icons: {
+    icon: 'favicon.ico'
+  }
+} 
 
 export default function RootLayout({
   children,
